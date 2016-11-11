37f7adb9d8f08b5aa7d49a4a15809b4e1b6aeab1
matheusgda <matheusgandrade@poli.ufrj.br>
2016-11-11 21:17:00 -0200

    add ROLE_ALUNO

--- src/main/webapp/app/admin/user-management/user-management-dialog.controller.js
+++ src/main/webapp/app/admin/user-management/user-management-dialog.controller.js
@@ -10,7 +10,7 @@
     function UserManagementDialogController ($stateParams, $uibModalInstance, entity, User, JhiLanguageService) {
         var vm = this;
 
-        vm.authorities = ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_SECRETARIO_ACADEMICO'];
+        vm.authorities = ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_SECRETARIO_ACADEMICO', 'ROLE_ALUNO'];
         vm.clear = clear;
         vm.languages = null;
         vm.save = save;
