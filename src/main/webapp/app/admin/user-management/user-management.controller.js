e8037254ca361378e81f5fb9e84fc1e4472aa19a
matheusgda <matheusgandrade@poli.ufrj.br>
2016-11-11 21:37:09 -0200

    fix ROLE_ALUNO

--- src/main/webapp/app/admin/user-management/user-management.controller.js
+++ src/main/webapp/app/admin/user-management/user-management.controller.js
@@ -10,7 +10,7 @@
     function UserManagementController(Principal, User, ParseLinks, AlertService, $state, pagingParams, paginationConstants, JhiLanguageService) {
         var vm = this;
 
-        vm.authorities = ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_SECRETARIO_ACADEMICO'];
+        vm.authorities = ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_SECRETARIO_ACADEMICO', 'ROLE_ALUNO'];
         vm.currentAccount = null;
         vm.languages = null;
         vm.loadAll = loadAll;
