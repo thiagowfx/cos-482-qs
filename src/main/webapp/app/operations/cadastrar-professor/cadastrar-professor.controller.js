23741c02a4bac0bae62b2e8fcf67827df0b78f68
Thiago Perrotta <tbperrotta@gmail.com>
2016-11-22 02:34:52 -0200

    save button almost works for cadatrar professor

--- src/main/webapp/app/operations/cadastrar-professor/cadastrar-professor.controller.js
+++ src/main/webapp/app/operations/cadastrar-professor/cadastrar-professor.controller.js
@@ -10,11 +10,30 @@
     function CadastrarProfessorController ($window, $scope, $state, Professor) {
         var vm = this;
 
+        // vm.professor = entity;
         vm.clear = clear;
+        vm.save = save;
 
         function clear() {
             // $window.document.getElementById('cadastrar-professor-name').value = "";
             // $window.document.getElementById('cadastrar-professor-siape').value = "";
+        }
+
+        function save() {
+            console.log('save');
+            vm.isSaving = true;
+            Professor.save(vm.professor, onSaveSuccess, onSaveError);
+        }
+
+        function onSaveSuccess (result) {
+            console.log('onSaveSuccess');
+            // $scope.$emit('cos482App:professorUpdate', result);
+            vm.isSaving = false;
+        }
+
+        function onSaveError () {
+            console.log('onSaveError');
+            vm.isSaving = false;
         }
     }
 })();
