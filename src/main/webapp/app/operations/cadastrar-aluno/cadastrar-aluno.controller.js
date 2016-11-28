0ff834b23feb330619551da3fcab0c75206914d4
Felipe Podolan Oliveira <felipe_po@poli.ufrj.br>
2016-11-28 16:18:44 -0200

    Adapt JHipster's way of creating new User. Id not being returned

--- src/main/webapp/app/operations/cadastrar-aluno/cadastrar-aluno.controller.js
+++ src/main/webapp/app/operations/cadastrar-aluno/cadastrar-aluno.controller.js
@@ -5,9 +5,9 @@
         .module('cos482App')
         .controller('CadastrarAlunoController', CadastrarAlunoController);
 
-    CadastrarAlunoController.$inject = ['$window', '$scope', '$state', '$translate', 'aluno_entity', 'aluno_mestrado_entity', 'aluno_doutorado_entity', 'usuario_entity', 'user_entity', 'Aluno', 'AlunoMestrado', 'AlunoDoutorado', 'User', 'Usuario'];
+    CadastrarAlunoController.$inject = ['$window', '$scope', '$state', '$translate', 'aluno_entity', 'aluno_mestrado_entity', 'aluno_doutorado_entity', 'usuario_entity', 'user_entity', 'Aluno', 'AlunoMestrado', 'AlunoDoutorado', 'Auth', 'LoginService', 'Usuario'];
 
-    function CadastrarAlunoController ($window, $scope, $state, $translate, aluno_entity, aluno_mestrado_entity, aluno_doutorado_entity, usuario_entity, user_entity, Aluno, AlunoMestrado, AlunoDoutorado, User, Usuario) {
+    function CadastrarAlunoController ($window, $scope, $state, $translate, aluno_entity, aluno_mestrado_entity, aluno_doutorado_entity, usuario_entity, user_entity, Aluno, AlunoMestrado, AlunoDoutorado, Auth, LoginService, Usuario) {
         var vm = this;
 
         vm.clear = clear;
@@ -17,12 +17,21 @@
         vm.alunodoutorado = aluno_doutorado_entity;
         vm.aluno = aluno_entity;
         vm.usuario = usuario_entity;
-        vm.user = user_entity;
+        
+        //user related vm's
+        vm.doNotMatch = null;
+        vm.error = null;
+        vm.errorUserExists = null;
+        vm.login = LoginService.open;
+        vm.registerAccount = {};
+        vm.success = null;
 
         function clear() {
         	$window.document.getElementById('cadastrar-aluno-type').selectedIndex = -1;
             $window.document.getElementById('cadastrar-aluno-login').value = "";
             $window.document.getElementById('cadastrar-aluno-email').value = "";
+            $window.document.getElementById('cadastrar-aluno-password').value = "";
+            $window.document.getElementById('cadastrar-aluno-confirmpassword').value = "";
             $window.document.getElementById('cadastrar-aluno-name').value = "";
             $window.document.getElementById('cadastrar-aluno-cpf').value = "";
             $window.document.getElementById('cadastrar-aluno-rg').value = "";
@@ -35,10 +44,46 @@
             vm.aluno = aluno_entity;
             vm.usuario = usuario_entity;
             vm.user = user_entity;
+            
+            //user related vm's
+            vm.doNotMatch = null;
+            vm.error = null;
+            vm.errorUserExists = null;
+            vm.login = LoginService.open;
+            vm.registerAccount = {};
+            vm.success = null;
         }
 
         function save() {
             vm.isSaving = true;
+            
+            if (vm.registerAccount.password !== vm.confirmPassword) {
+                vm.doNotMatch = 'ERROR';
+            } else {
+            	vm.registerAccount.langKey = $translate.use();
+                vm.doNotMatch = null;
+                vm.error = null;
+                vm.errorUserExists = null;
+                vm.errorEmailExists = null;
+
+                Auth.createAccount(vm.registerAccount).then(function (qqq) {
+                    vm.success = 'OK';
+                    qqq.$promise.then(function(www){
+                    	console.log(www);
+                    	console.log(www.id);
+                    });
+                    console.log(qqq); //Find a way to get the id
+                }).catch(function (response) {
+                    vm.success = null;
+                    if (response.status === 400 && response.data === 'login already in use') {
+                        vm.errorUserExists = 'ERROR';
+                    } else if (response.status === 400 && response.data === 'e-mail address already in use') {
+                        vm.errorEmailExists = 'ERROR';
+                    } else {
+                        vm.error = 'ERROR';
+                    }
+                });
+            }
             
             if($window.document.getElementById('cadastrar-aluno-type').selectedIndex === 0) {
             	Usuario.save(vm.usuario, function(){}, function(){}).$promise.then(function(usuario) {
