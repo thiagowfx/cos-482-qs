defd9806ad69fcb7b33c0b231003613bb43c9966
matheusgda <matheusgandrade@poli.ufrj.br>
2016-12-04 20:26:08 -0200

    get values for documentos in enviar-documento

--- src/main/webapp/app/operations/enviar-documento-mestrado/enviar-documento-mestrado.controller.js
+++ src/main/webapp/app/operations/enviar-documento-mestrado/enviar-documento-mestrado.controller.js
@@ -5,9 +5,6 @@
         .module('cos482App')
         .directive("fileread", [function () {
             return {
-                scope: {
-                    fileread: "="
-                },
                 link: function (scope, element, attributes) {
                     element.bind("change", function (changeEvent) {
                         scope.$apply(function () {
@@ -24,9 +21,15 @@
     function EnviarDocumentoMestradoController ($window, $scope, $state, $translate, DocumentoSistema, LogDoSistema, log_entity, Principal, Usuario, Aluno, AlunoMestrado) {
         var vm = this;
         vm.alunoData = {};
+        
         vm.saveDiplomaGraduacao = saveDiplomaGraduacao;
+        vm.saveDeclaracaoConclusao = saveDeclaracaoConclusao;
+        vm.saveCertidaoColacao = saveCertidaoColacao;
+        vm.saveHistoricoGraduacao = saveHistoricoGraduacao;
+        vm.saveCertidaoConclusao = saveCertidaoConclusao;
 
         vm.log = log_entity;
+
 
 
         getUserName();
@@ -40,6 +43,64 @@
             console.log(JSON.stringify(vm.alunoData));
         }
 
+        function saveDeclaracaoConclusao() {
+            console.log(vm.declaracaoConclusao);
+            console.log(vm.declaracaoConclusao.name);
+        }
+
+        function saveCertidaoColacao() {
+            console.log(vm.certidaoColacao);
+            console.log(vm.certidaoColacao.name);
+        }
+
+        function saveHistoricoGraduacao() {
+            console.log(vm.historicoGraduacao);
+            console.log(vm.historicoGraduacao.name);
+        }
+
+        function saveCertidaoConclusao() {
+            console.log(vm.certidaoConclusao);
+            console.log(vm.certidaoConclusao.name);
+        }
+
+        function saveDiplomaGraduacao() {
+            console.log(vm.diplomaGraduacao);
+            console.log(vm.diplomaGraduacao.name);
+        }
+
+        function filePicker() {
+            var picker = document.getElementById("enviar-documento-mestrado-historico-graduacao");
+            var txt = "";
+            console.log("Here I am!!");
+            if ('files' in picker) {
+                var file = picker.files[0];
+                console.log(JSON.file);
+            //     if (x.files.length == 0) {
+            //         txt = "Select one or more files.";
+            //     } else {
+            //         for (var i = 0; i < x.files.length; i++) {
+            //             txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+            //             var file = x.files[i];
+            //             if ('name' in file) {
+            //                 txt += "name: " + file.name + "<br>";
+            //             }
+            //             if ('size' in file) {
+            //                 txt += "size: " + file.size + " bytes <br>";
+            //             }
+            //         }
+            //     }
+            // } 
+            // else {
+            //     if (x.value == "") {
+            //         txt += "Select one or more files.";
+            //     } else {
+            //         txt += "The files property is not supported by your browser!";
+            //         txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
+            //     }
+            // }
+        }
+            
+        }
         function getUserName() {
             var account = {};
             Principal.identity().then(function(account) {
@@ -66,11 +127,6 @@
                 // );
             }(account));
             console.log(JSON.stringify(account));
-        }
-
-        function saveDiplomaGraduacao() {
-            console.log(vm.diplomaGraduacao);
-            console.log(vm.diplomaGraduacao.name);
         }
 
         function getUsuarioId (){
